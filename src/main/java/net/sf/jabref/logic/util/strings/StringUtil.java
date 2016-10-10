8e0b1b25875d9d72a749b214148088717c815e45
Sascha Zeller <zeller.dev@gmail.com>
2016-10-10 23:29:30 +0200

    move bibsonomy/StringUtil to jabref/StringUtil

--- src/main/java/net/sf/jabref/logic/util/strings/StringUtil.java
+++ src/main/java/net/sf/jabref/logic/util/strings/StringUtil.java
@@ -1,5 +1,6 @@
 package net.sf.jabref.logic.util.strings;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -671,6 +672,21 @@ public class StringUtil {
             return "<b>" + alternative + "</b>";
         }
         return "<b>" + input + "</b>";
+    }
+
+    /**
+     * Encodes a string to UTF8
+     *
+     * @param s the string which should be encoded
+     * @return the encoded string or null
+     */
+    public static String toUTF8(String s) {
+        if (s != null) {
+            // FIXME: what is this? why do we want to introduce platform dependency here?
+            // This should only be correct if an error from somewhere else has to be corrected.
+            return new String(s.getBytes(StandardCharsets.UTF_8));
+        }
+        return null;
     }
 
 }
